20e1658466ff1c0e1ebf633523d849822dd710bf
pamput <pamput@gmail.com>
2015-08-19 17:37:41 +0200

    Light server version

--- server/app.js
+++ server/app.js
@@ -1,11 +1,7 @@
 var http = require('http');
 var express = require('express');
-var main = require('./route/main');
-var contacts = require('./route/contacts');
 
 var app = express();
-app.use("/", main);
-app.use("/contacts", contacts);
 
 app.use(express.static(__dirname + '/static'));
 
--- server/utils/redis-client.js
+++
@@ -1,9 +0,0 @@
-var redis = require("redis"),
-    config = require("../config");
-    //client = redis.createClient(
-    //    config.redis.port,
-    //    config.redis.host,
-    //    config.redis.options
-    //);
-
-module.exports = null;
